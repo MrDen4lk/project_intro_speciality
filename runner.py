5ea76aecacc61f209a3bf8fc48325b39951f0dd6
MrDen4lk <OffEvstigneevDA@yandex.ru>
2024-11-08 10:49:58 +0300

    clear

--- runner.py
+++
@@ -1,9 +0,0 @@
-import telegram_bot
-
-def main():
-    bot = telegram_bot.Bot()
-
-    telegram_bot.Bot.run(bot)
-
-if __name__ == "__main__":
-    main()
--- telegram_bot.py
+++
@@ -1,9 +0,0 @@
-import requests_to_database
-
-class Bot:
-
-    def __init__(self):
-        pass
-
-    def run(self):
-        pass
